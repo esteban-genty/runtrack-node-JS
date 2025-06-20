8337830fcdb1d305987523f1efe5fbe5b31f58d9
esteban-genty <esteban.genty@laplateforme.io>
2025-06-20 17:23:04 +0200

    Ajout du JOB04 du JOUR05

--- JOUR05/runtrack_express/projet_web/index.js
+++ JOUR05/runtrack_express/projet_web/index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 80;
 
+app.get('/', (req, res) => {
+  res.send("Page d'accueil");
+})
+
+app.get('/about', (req, res) => {
+  res.send('Page à propos');
+})
+
 app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
 });
