4c5f9a27d955d09da26f4e28ce466fd8afbfb201
esteban-genty <esteban.genty@laplateforme.io>
2025-06-20 17:50:37 +0200

    Ajout du JOB07 du JOUR05

--- JOUR05/runtrack_express/projet_web/index.js
+++ JOUR05/runtrack_express/projet_web/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 const port = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/index.html'));
+    res.status(200).sendFile(path.join(__dirname, './views/index.html'));
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/about.html'));
+    res.status(200).sendFile(path.join(__dirname, './views/about.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).send("<div>404 Not Found</div>");
 });
 
 app.listen(port, () => {
